f6b1d5f64d1e1c6fc321c7f27e3166dabbf2eb46
Harshada JagtapW <jagtap.harshada.11@gmail.com>
2024-12-14 11:51:57 +0530

    Branch B changes

--- src/Harshada/Test2.java
+++ src/Harshada/Test2.java
@@ -2,6 +2,7 @@ package Harshada;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("Hello_class2");
+		System.out.println("Hello_clas2_BranchB");
+		System.out.println("Hello_class_BRANCHB");
 	}
 }
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module TestNov24_Harshada {
-}
