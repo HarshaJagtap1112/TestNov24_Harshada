2275e3c4636adced96d560315e99335742c26cc2
Harshada JagtapW <jagtap.harshada.11@gmail.com>
2024-12-23 01:15:34 +0530

    Fixd pattern printing

--- src/Harshada/PatternPrint.java
+++ src/Harshada/PatternPrint.java
@@ -7,11 +7,12 @@ package Harshada;
 
 public class PatternPrint {
 	static	char ch='A';
+	static int a=0;
  static	void printPatterm() {
 		for(int i=1;i<=4;i++) {
 			for(int j=1;j<=i;j++) {
 				System.out.print(i+""+ch+" ");
-				i++;
+				a++;
 				ch++;
 			}System.out.println();
 		}
